1dc0b7e5d9631f013282532685f83b643e4c92f3
MF57 <p-bochenek@outlook.com>
2017-04-17 20:29:02 +0200

    trace endpoint init

--- central.py
+++ central.py
@@ -8,5 +8,12 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/trace/<src>/<dst>')
+def trace(src, dst):
+    print src
+    print dst
+    return 'Hello World!'
+
+
 if __name__ == '__main__':
     app.run()
